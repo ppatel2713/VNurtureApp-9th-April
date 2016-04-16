94ab367768874a6e4f76b8437efdbaaf62d9f5b7
Prachi Patel <ppatel2713@gmail.com>
2016-04-16 11:09:32 +0530

    Back Button Implemented

--- VNurture App/VNurture App/Tollbar.m
+++ VNurture App/VNurture App/Tollbar.m
@@ -13,13 +13,13 @@
 -(void)awakeFromNib
 {
     [self setBarTintColor :[UIColor colorWithRed:63.0/255.0 green:81.0/255.0 blue:181.0/255.0 alpha:1.0]];
-   UIBarButtonItem *back_button = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStyleDone target:nil action:nil];
-    [ back_button setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
-                                        [UIFont fontWithName:@"Noteworthy" size:18.0], NSFontAttributeName,
-                                        [UIColor whiteColor], NSForegroundColorAttributeName,[UIColor whiteColor],NSBackgroundColorAttributeName,
-                                        nil]
-                              forState:UIControlStateNormal];
-    self.items = [NSArray arrayWithObjects:back_button, nil];
+  // UIBarButtonItem *back_button = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStyleDone target:nil action:nil];
+//    [ back_button setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
+//                                        [UIFont fontWithName:@"Noteworthy" size:18.0], NSFontAttributeName,
+//                                        [UIColor whiteColor], NSForegroundColorAttributeName,[UIColor whiteColor],NSBackgroundColorAttributeName,
+//                                        nil]
+//                              forState:UIControlStateNormal];
+   // self.items = [NSArray arrayWithObjects:back_button, nil];
 }
 
 @end
